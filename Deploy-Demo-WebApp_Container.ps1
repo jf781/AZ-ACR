3c09b415c0abea6b782bd7c9368b80a56b278305
jf781 <50719731+jf781@users.noreply.github.com>
2019-10-21 14:16:10 -0500

    Update Deploy-Demo-WebApp_Container.ps1

--- Deploy-Demo-WebApp_Container.ps1
+++ Deploy-Demo-WebApp_Container.ps1
@@ -2,6 +2,7 @@
 
 # Defines variable names
 $RGName = "AHD-LabRG01"
+$DockerImage = "privatebin/ahead"
 
 
 
@@ -10,16 +11,27 @@ $LabRG = New-AzResourceGroup -Name $RGName -Location 'Central US'
 
 # Create ACR
 New-AzResourceGroupDeployment -Name "AHD-Lab-ACR" `
-    -TemplateUri https://github.com/jf781/AZ-ACR/blob/master/acr-template.json `
+    -TemplateUri https://raw.githubusercontent.com/jf781/AZ-ACR/master/acr-template.json `
     -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-ACR/master/acr-template-parameters.json `
     -ResourceGroupName $LabRG.ResourceGroupName
 
 # Push docker image to ACR
 $ACR = Get-AzContainerRegistry -ResourceGroupName $RGName
 $Creds = Get-AzContainerRegistryCredential -Registry $ACR
-$creds.Password | docker login $ACR.LoginServer -u $creds.Username 
+$ACRPwd = $creds.Password 
+$ACRPwd | docker login $ACR.LoginServer -u $creds.Username --password-stdin
+$TagName = $acr.LoginServer + "/ahead/demo"
+docker tag $DockerImage $TagName
+docker push $TagName
 
-# Create AppService Plan
+# Create AppService and AppService Plan
+New-AzResourceGroupDeployment -Name "AHD-Lab-AppSvc" `
+    -TemplateUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template.json `
+    -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template-paramters.json `
+    -ResourceGroupName $LabRG.ResourceGroupName
 
-# Create Web App
+New-AzResourceGroupDeployment -Name "AHD-Lab-WebApp" `
+    -TemplateUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-acr-template.json `
+    -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-acr-template-paramters.json `
+    -ResourceGroupName $LabRG.ResourceGroupName
 
