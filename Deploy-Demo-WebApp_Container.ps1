a3386e6d8725fa9a9d202e1d0e9cbcf3ea7f8db7
jf781 <50719731+jf781@users.noreply.github.com>
2019-10-22 09:08:56 -0500

    Updated comments

--- Deploy-Demo-WebApp_Container.ps1
+++ Deploy-Demo-WebApp_Container.ps1
@@ -1,11 +1,21 @@
-##Deployment Script
+<# 
+Deployment Script that will push create the following items in the Azure subscription
+
+    - Resource Group defined in the "RGName" variable
+    - Azure Container Registry (ACR)
+    - App Service Plan
+    - Web App
+
+It will push a local docker image that you have defined in the "DockerName" variable to the ACR and set that as the
+containered used in the webapp
+
+AHEAD - Joe Fecht - October 2019
+
+#>
 
 # Defines variable names
 $RGName = "AHD-LabRG01"
 $DockerImage = "privatebin/ahead"
-
-
-
 
 # Create Resource Group
 $LabRG = New-AzResourceGroup -Name $RGName -Location 'Central US'
@@ -16,25 +26,31 @@ New-AzResourceGroupDeployment -Name "AHD-Lab-ACR" `
     -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-ACR/master/acr-template-parameters.json `
     -ResourceGroupName $LabRG.ResourceGroupName
 
-# Push docker image to ACR
+# Getting credentials and logging docker into ACR
 $ACR = Get-AzContainerRegistry -ResourceGroupName $RGName
 $Creds = Get-AzContainerRegistryCredential -Registry $ACR
-$ACRPwd = $creds.Password | ConvertTo-SecureString -AsPlainText -Force
 $Creds.Password | docker login $ACR.LoginServer -u $creds.Username --password-stdin
+
+# Storing creds in secure string variable
+$ACRPwd = $creds.Password | ConvertTo-SecureString -AsPlainText -Force
+
+#Clear creds variable stored in plain text
 $Creds = $null
+
+# Push docker image to ACR
 $TagName = $acr.LoginServer + "/ahead/demo"
 docker tag $DockerImage $TagName
 docker push $TagName
 
-# Create AppService and AppService Plan
+# Create AppService Plan
 New-AzResourceGroupDeployment -Name "AHD-Lab-AppSvc" `
     -TemplateUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template.json `
     -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template-parameters.json `
     -ResourceGroupName $LabRG.ResourceGroupName
 
-# Copy files to temp directory
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/jf781/AZ-AppService/master/WebApp/az-webapp-acr-template.json | Select-Object -ExpandProperty content | out-file /tmp/webapp-template.json
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/jf781/AZ-AppService/master/WebApp/az-webapp-acr-template-parameters.json | Select-Object -ExpandProperty content | out-file /tmp/webapp-template-parameters.json
+# Copy ARM template JSON files to temp directory
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/jf781/AZ-AppService/master/WebApp/az-webapp-acr-template.json | Select-Object -ExpandProperty content | out-file /tmp/webapp-template.json -Force
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/jf781/AZ-AppService/master/WebApp/az-webapp-acr-template-parameters.json | Select-Object -ExpandProperty content | out-file /tmp/webapp-template-parameters.json -Force
 
 # Deploy WebApp
 New-AzResourceGroupDeployment -Name "AHD-Lab-WebApp" `
