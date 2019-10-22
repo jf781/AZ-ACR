1e68a67e89d75e04f597f16424d31d7dc1f0f143
jf781 <50719731+jf781@users.noreply.github.com>
2019-10-22 08:17:54 -0500

    Updated comments

--- Deploy-Demo-WebApp_Container.ps1
+++ Deploy-Demo-WebApp_Container.ps1
@@ -1,15 +1,16 @@
 ##Deployment Script
 
 # Defines variable names
-$RGName = "AHD-LabRG01"
+$RGName = "AHD-LabRG03"
 $DockerImage = "privatebin/ahead"
+
 
 
 
 # Create Resource Group
 $LabRG = New-AzResourceGroup -Name $RGName -Location 'Central US'
 
-# Create ACR
+# Create Azure Container Registry (ACR)
 New-AzResourceGroupDeployment -Name "AHD-Lab-ACR" `
     -TemplateUri https://raw.githubusercontent.com/jf781/AZ-ACR/master/acr-template.json `
     -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-ACR/master/acr-template-parameters.json `
@@ -18,8 +19,8 @@ New-AzResourceGroupDeployment -Name "AHD-Lab-ACR" `
 # Push docker image to ACR
 $ACR = Get-AzContainerRegistry -ResourceGroupName $RGName
 $Creds = Get-AzContainerRegistryCredential -Registry $ACR
-$ACRPwd = $creds.Password 
-$ACRPwd | docker login $ACR.LoginServer -u $creds.Username --password-stdin
+$ACRPwd = $creds.Password | ConvertTo-SecureString -AsPlainText -Force
+$Creds.pw | docker login $ACR.LoginServer -u $creds.Username --password-stdin
 $TagName = $acr.LoginServer + "/ahead/demo"
 docker tag $DockerImage $TagName
 docker push $TagName
@@ -27,11 +28,20 @@ docker push $TagName
 # Create AppService and AppService Plan
 New-AzResourceGroupDeployment -Name "AHD-Lab-AppSvc" `
     -TemplateUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template.json `
-    -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template-paramters.json `
+    -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-linux-template-parameters.json `
     -ResourceGroupName $LabRG.ResourceGroupName
 
+# Copy files to temp directory
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-acr-template.json | Select-Object -ExpandProperty content | out-file /tmp/webapp-template.json
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-acr-template-parameters.json | Select-Object -ExpandProperty content | out-file /tmp/webapp-template-parameters.json
+
+# Deploy WebApp
 New-AzResourceGroupDeployment -Name "AHD-Lab-WebApp" `
-    -TemplateUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-acr-template.json `
-    -TemplateParameterUri https://raw.githubusercontent.com/jf781/AZ-AppService/master/AppSvc/az-appsvc-acr-template-paramters.json `
-    -ResourceGroupName $LabRG.ResourceGroupName
+    -TemplateFile /tmp/webapp-template.json `
+    -TemplateParameterFile /tmp/webapp-template-parameters.json `
+    -ResourceGroupName $LabRG.ResourceGroupName `
+    -registryPassword (ConvertTo-SecureString -AsPlainText -Force -String $ACRPwd)
 
+# Clean up temp files
+rm /tmp/webapp-template.json
+rm /tmp/webapp-template-parameters.json
