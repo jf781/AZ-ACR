4e0103b4480eba7f2267f49d075a70ba7f334d77
jf781 <50719731+jf781@users.noreply.github.com>
2021-02-22 18:52:15 -0600

    Update new-relic.ps1

--- new-relic.ps1
+++ new-relic.ps1
@@ -1,5 +1,5 @@
 new-item -type directory -path C:\Users\Administrator\Desktop\Test 
-msiexec.exe /i "C:\NR\newrelic-infra.msi"
+# msiexec.exe /i "C:\NR\newrelic-infra.msi"
 
-@echo on
-C:\Program Files\New Relic\newrelic-infra\newrelic-infra-service.exe
+# @echo on
+# C:\Program Files\New Relic\newrelic-infra\newrelic-infra-service.exe
